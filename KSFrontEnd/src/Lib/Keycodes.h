c9d4f7236ae0e19ce16e9e3bac43ca80bc4a848d
August Dahlkvist <AugustDahlkvist@gmail.com>
2026-04-04 17:24:45 +0100

    Add header with every keycode

---
+++ KSFrontEnd/src/Lib/Keycodes.h
@@ -0,0 +1,122 @@
+// ============================================================================
+// Constants for every key code
+// ----------------------------------------------------------------------------
+#pragma once
+// ============================================================================
+// Include
+// ============================================================================
+
+// Row 0
+#define KS_KEYCODE_ESC 0x29
+#define KS_KEYCODE_F1 0x3A
+#define KS_KEYCODE_F2 0x3B
+#define KS_KEYCODE_F3 0x3C
+#define KS_KEYCODE_F4 0x3D
+#define KS_KEYCODE_F5 0x3E
+#define KS_KEYCODE_F6 0x3F
+#define KS_KEYCODE_F7 0x40
+#define KS_KEYCODE_F8 0x41
+#define KS_KEYCODE_F9 0x42
+#define KS_KEYCODE_F10 0x43
+#define KS_KEYCODE_F11 0x44
+#define KS_KEYCODE_F12 0x45
+#define KS_KEYCODE_PRINT 0x46
+#define KS_KEYCODE_SCROLL 0x47
+#define KS_KEYCODE_PAUSE 0x48
+
+// Row 1
+#define KS_KEYCODE_TILDE 0x35
+#define KS_KEYCODE_1 0x1E
+#define KS_KEYCODE_2 0x1F
+#define KS_KEYCODE_3 0x20
+#define KS_KEYCODE_4 0x21
+#define KS_KEYCODE_5 0x22
+#define KS_KEYCODE_6 0x23
+#define KS_KEYCODE_7 0x24
+#define KS_KEYCODE_8 0x25
+#define KS_KEYCODE_9 0x26
+#define KS_KEYCODE_0 0x27
+#define KS_KEYCODE_HYPHEN 0x2D
+#define KS_KEYCODE_EQUALS 0x2E
+#define KS_KEYCODE_BACKSPACE 0x2A
+#define KS_KEYCODE_INSERT 0x49
+#define KS_KEYCODE_HOME 0x4A
+#define KS_KEYCODE_PG_UP 0x4B
+#define KS_KEYCODE_NUMLOCK 0x53
+#define KS_KEYCODE_NUM_DIVIDE 0x54
+#define KS_KEYCODE_NUM_TIMES 0x55
+#define KS_KEYCODE_NUM_MINUS 0x56
+
+// Row 2
+#define KS_KEYCODE_TAB 0x2B
+#define KS_KEYCODE_Q 0x14
+#define KS_KEYCODE_W 0x1A
+#define KS_KEYCODE_E 0x08
+#define KS_KEYCODE_R 0x15
+#define KS_KEYCODE_T 0x17
+#define KS_KEYCODE_Y 0x1C
+#define KS_KEYCODE_U 0x18
+#define KS_KEYCODE_I 0x0C
+#define KS_KEYCODE_O 0x12
+#define KS_KEYCODE_P 0x13
+#define KS_KEYCODE_BRACE_SQR_LEFT 0x2F
+#define KS_KEYCODE_BRACE_SQR_RIGHT 0x30
+#define KS_KEYCODE_BACK_SLASH 0x31
+#define KS_KEYCODE_DELETE 0x4C
+#define KS_KEYCODE_END 0x4D
+#define KS_KEYCODE_PG_DOWN 0x4E
+#define KS_KEYCODE_NUM_7 0x5F
+#define KS_KEYCODE_NUM_8 0x60
+#define KS_KEYCODE_NUM_9 0x61
+#define KS_KEYCODE_NUM_PLUS 0x57
+
+// Row 3
+#define KS_KEYCODE_CAPS_LOCK 0x39
+#define KS_KEYCODE_A 0x04
+#define KS_KEYCODE_S 0x16
+#define KS_KEYCODE_D 0x07
+#define KS_KEYCODE_F 0x09
+#define KS_KEYCODE_G 0x0A
+#define KS_KEYCODE_H 0x0B
+#define KS_KEYCODE_J 0x0D
+#define KS_KEYCODE_K 0x0E
+#define KS_KEYCODE_L 0x0F
+#define KS_KEYCODE_SEMI_COLON 0x33
+#define KS_KEYCODE_QUOTE 0x34
+#define KS_KEYCODE_ENTER 0x28
+#define KS_KEYCODE_NUM_4 0x5C
+#define KS_KEYCODE_NUM_5 0x5D
+#define KS_KEYCODE_NUM_6 0x5E
+
+// Row 4
+#define KS_KEYCODE_L_SHIFT 0x68
+#define KS_KEYCODE_Z 0x1D
+#define KS_KEYCODE_X 0x1B
+#define KS_KEYCODE_C 0x06
+#define KS_KEYCODE_V 0x19
+#define KS_KEYCODE_B 0x05
+#define KS_KEYCODE_N 0x11
+#define KS_KEYCODE_M 0x10
+#define KS_KEYCODE_COMMA 0x36
+#define KS_KEYCODE_PERIOD 0x37
+#define KS_KEYCODE_FORWARD_SLASH 0x38
+#define KS_KEYCODE_R_SHIFT 0x6C
+#define KS_KEYCODE_UP_ARROW 0x52
+#define KS_KEYCODE_NUM_1 0x59
+#define KS_KEYCODE_NUM_2 0x5A
+#define KS_KEYCODE_NUM_3 0x5B
+#define KS_KEYCODE_NUM_ENTER 0x58
+
+// Row 5
+#define KS_KEYCODE_L_CTRL 0x67
+#define KS_KEYCODE_WIN 0x6A
+#define KS_KEYCODE_L_ALT 0x69
+#define KS_KEYCODE_SPACE 0x2C
+#define KS_KEYCODE_R_ALT 0x6D
+#define KS_KEYCODE_MENU 0x65
+#define KS_KEYCODE_R_CTRL 0x6B
+#define KS_KEYCODE_LEFT_ARROW 0x50
+#define KS_KEYCODE_DOWN_ARROW 0x51
+#define KS_KEYCODE_RIGHT_ARROW 0x4F
+#define KS_KEYCODE_NUM_0 0x62
+#define KS_KEYCODE_NUM_PERIOD 0x63
